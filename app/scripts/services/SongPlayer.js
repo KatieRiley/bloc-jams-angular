73697b94c43a30627a8e92736a6d3dead0ecd3ef
Katie Riley <katieAnRiley@gmail.com>
2016-10-05 21:14:18 -0700

    services part 2 checkpoint

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,9 +1,15 @@
 (function() {
     function SongPlayer() {
+        /**
+        * @desc Song Player object
+        * @type {Object}
+        */
         var SongPlayer = {};
-        
+        /**
+        * @desc current song being played/paused
+        * @type {Object}
+        */
         var currentSong = null;
-        
         /**
         * @desc Buzz object audio file
         * @type {Object}
@@ -29,14 +35,33 @@
             currentSong = song;
         };
         
+        /**
+        * @function playSong
+        * @desc Starts to play the audio file and sets the playing property of song.playing to true
+        */
+        var playSong = function(){
+            currentBuzzObject.play();
+            setSong(song);
+            
+        }
+        
+        /**
+        * @function play
+        * @desc sets a song to currentSong and starts to play the audio file
+        * @param {Object} song
+        */
         SongPlayer.play = function(song) {
             if (currentSong !== song) {
-                setSong(song);
-                currentBuzzObject.play();
+                playSong();
                 song.playing = ture;
             }
         };
         
+        /**
+        * @function pause
+        * @desc Pauses currently playing song and sets song.playing to false
+        * @param {Object} song
+        */
         SongPlayer.pause = function(song) {
             currentBuzzObject.pause();
             song.playing = false;
